94d81458f2381fb4ee70f58bea77c5fd8e67cf0a
D7703_07 <d7703_07@lulzsec.local>
2019-03-19 15:28:35 +0900

    button 3 add

--- Change Color/ViewController.swift
+++ Change Color/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLable: UILabel!
+    var colorState = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        myLable.text = "안녕 또 눌렸냐?"
+    }
+    
 
-
+    @IBAction func label_Clear(_ sender: UIButton) {
+        myLable.text = ""
+    }
+    
+    
+    @IBAction func changeViewColor(_ sender: UIButton) {
+        if colorState == true {
+            self.view.backgroundColor = UIColor.yellow
+            colorState = false
+        }
+        else{
+            self.view.backgroundColor = UIColor.white
+            colorState = true
+        }
+    }
+    
 }
 
